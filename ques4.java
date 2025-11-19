04cd9adc659e19ab66797fbf252be1d7fb01602c
assasasasass <aakritisainju001@gmail.com>
2025-11-19 11:10:34 +0545

    quesno4

---
+++ ques4.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class ques4 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ques4{
+    public static void main(String[] args) {
+
+        long bigNumber = 9876543210L;
+
+        float floatNumber = 12.34f;
+
+        char copyrightSymbol = '\u00A9';
+
+        System.out.println("Long value: " + bigNumber);
+        System.out.println("Float value: " + floatNumber);
+        System.out.println("Unicode char value: " + copyrightSymbol);
+    }
+}
