914ee3ae0be342a7640a77e65af9ee6e2c18e9ff
assasasasass <aakritisainju001@gmail.com>
2025-11-26 09:15:29 +0545

    workshop4

---
+++ week4tutorial.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+ * Write a description of class week4tutorial here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class week4tutorial
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int age=sc.nextInt();
+        
+        if(age>18){
+            System.out.println("Adult");
+        }
+        else{
+            System.out.println("less than 18");
+            
+        }
+        }}
+    
---
+++ workshop4.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class workshop4 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class workshop4
+{
+    public static void main(String[] args)
+    {
+        int choice = 1; //[1, 2, 3]
+        
+        switch(choice)
+        {
+          case 1: System.out.println("Drink: Pepsi");
+          case 2: System.out.println("Drink: Sprite");
+          case 3: System.out.println("Drink: Fanta");
+          default: System.out.println("Not a valid choice");
+          
+        }
+    }
+}
